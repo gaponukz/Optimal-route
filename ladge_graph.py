65fa03cbae7558792325301611aebbdbc9360924
gaponukz <49754258+gaponukz@users.noreply.github.com>
2022-01-04 21:06:47 +0200

    Create ladge_graph.py

---
+++ ladge_graph.py
@@ -0,0 +1,26 @@
+from search_graph import *
+
+ladge_graph: Graph = {
+    "start": {
+        "a": 5,
+        "b": 2
+    },
+    "a": {
+        "d": 4,
+        "c": 2
+    },
+    "b": {
+        "a": 8,
+        "c": 7
+    },
+    "d": {
+        "c": 6,
+        "end": 2
+    },
+    "c": {
+        "end": 1
+    },
+    "end": {}
+}
+
+print(get_optimal_route_cost(ladge_graph))
